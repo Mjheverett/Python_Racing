6143fbb49b0a060cf927ca3ae2bfe183bc718797
Matthew Everett <matthew@mjheverett.com>
2020-08-05 16:56:08 -0400

    fix looping error in main menu

--- game.py
+++ game.py
@@ -68,44 +68,44 @@ def get_user_choice(choice_list):
 
 def game_startmenu():
     while True:
-        choice = get_user_choice(game_menu)
-        if choice == 1:
-            choice = get_user_choice(driver_menu)
-            if choice == 1:
+        menu_choice = get_user_choice(game_menu)
+        if menu_choice == 1:
+            driver_choice = get_user_choice(driver_menu)
+            if driver_choice == 1:
                 players_driver = Driver_1("Veteran", 5, 10, 5, 5, 10)
-            if choice == 2:
+            if driver_choice == 2:
                 players_driver = Driver_2("Crafty", 0, 10, 10, 10, 5)
-            if choice == 3:
+            if driver_choice == 3:
                 players_driver = Driver_3("Cavalier", 10, 0, 5, 5, 5)
-            if choice == 4:
+            if driver_choice == 4:
                 players_driver = Driver_4("Joe", 5, 5, 5, 5, 5)
-        if choice == 2:
-            choice = get_user_choice(kart_menu)
-            if choice == 1:
+        if menu_choice == 2:
+            kart_choice = get_user_choice(kart_menu)
+            if kart_choice == 1:
                 players_kart = Standard_kart("Standard", 6.5, 4, 9, 6.5)
-            if choice == 2:
+            if kart_choice == 2:
                 players_kart = Mushmellow_kart("Mushmellow", 4.3, 6.9, 6.9, 8.7)
-            if choice == 3:
+            if kart_choice == 3:
                 players_kart = Powerflower_kart("Powerflower", 6.7, 4.3, 9, 8.7)
-            if choice == 4:
+            if kart_choice == 4:
                 players_kart = Drybomber_kart("Dry Bomber", 3.4, 9.8, 3.9, 9.5)
-        if choice == 3:
-            choice = get_user_choice(track_menu)
-            if choice == 1:
+        if menu_choice == 3:
+            track_choice = get_user_choice(track_menu)
+            if track_choice == 1:
                 players_track = Track_1("Suzuka", 3, 0, 8, 8)
-            if choice == 2:
+            if track_choice == 2:
                 players_track = Track_2("Fuji", 5, 8, 4, 3)
-            if choice == 3:
+            if track_choice == 3:
                 players_track = Track_3("Inagawa", 8, 5, 3, 5)
-            if choice == 4:
+            if track_choice == 4:
                 players_track = Track_4("Tsukuba", 9, 0, 7, 4)
-        if choice == 4:
+        if menu_choice == 4:
             print(players_driver.stats())
             print(players_kart.stats())
             print(players_track.stats())
-        if choice == 5:
+        if menu_choice == 5:
             game(players_driver, players_kart, players_track)
-        if choice == 6:
+        if menu_choice == 6:
             exit_prompt = input("Are you sure you want to quit? All game data will be lost. (Y or N): ").upper()
             if exit_prompt == "Y":
                 break
